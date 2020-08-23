63842a6f008e3496b548ac0375d816b88277ec9b
amfateme <56359249+amfateme@users.noreply.github.com>
2020-08-24 00:19:52 +0430

    Update main.go

--- main.go
+++ main.go
@@ -1,151 +1,135 @@
 /*
-SPDX-License-Identifier: Apache-2.0
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 	"strconv"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// SmartContract provides functions for managing a car
-type SmartContract struct {
+// ABstore Chaincode implementation
+type ABstore struct {
 	contractapi.Contract
 }
 
-// Car describes basic details of what makes up a car
-type Car struct {
-	Make   string `json:"make"`
-	Model  string `json:"model"`
-	Colour string `json:"colour"`
-	Owner  string `json:"owner"`
-}
-
-// QueryResult structure used for handling result of query
-type QueryResult struct {
-	Key    string `json:"Key"`
-	Record *Car
-}
-
-// InitLedger adds a base set of cars to the ledger
-func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	cars := []Car{
-		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
-		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
-		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
-		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
-		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
-		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
-		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
-		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
-		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
-		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
+func (t *ABstore) Init(ctx contractapi.TransactionContextInterface, A string, Aval int, B string, Bval int) error {
+	fmt.Println("ABstore Init")
+	var err error
+	// Initialize the chaincode
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+	// Write the state to the ledger
+	err = ctx.GetStub().PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return err
 	}
 
-	for i, car := range cars {
-		carAsBytes, _ := json.Marshal(car)
-		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
-
-		if err != nil {
-			return fmt.Errorf("Failed to put to world state. %s", err.Error())
-		}
+	err = ctx.GetStub().PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-// CreateCar adds a new car to the world state with given details
-func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
-	car := Car{
-		Make:   make,
-		Model:  model,
-		Colour: colour,
-		Owner:  owner,
-	}
-
-	carAsBytes, _ := json.Marshal(car)
-
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
-}
-
-// QueryCar returns the car stored in the world state with given id
-func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
-	carAsBytes, err := ctx.GetStub().GetState(carNumber)
-
+// Transaction makes payment of X units from A to B
+func (t *ABstore) Invoke(ctx contractapi.TransactionContextInterface, A, B string, X int) error {
+	var err error
+	var Aval int
+	var Bval int
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	Avalbytes, err := ctx.GetStub().GetState(A)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
+		return fmt.Errorf("Failed to get state")
 	}
-
-	if carAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", carNumber)
+	if Avalbytes == nil {
+		return fmt.Errorf("Entity not found")
 	}
+	Aval, _ = strconv.Atoi(string(Avalbytes))
 
-	car := new(Car)
-	_ = json.Unmarshal(carAsBytes, car)
-
-	return car, nil
-}
-
-// QueryAllCars returns all cars found in world state
-func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
-	startKey := ""
-	endKey := ""
-
-	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
-
+	Bvalbytes, err := ctx.GetStub().GetState(B)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("Failed to get state")
 	}
-	defer resultsIterator.Close()
-
-	results := []QueryResult{}
-
-	for resultsIterator.HasNext() {
-		queryResponse, err := resultsIterator.Next()
-
-		if err != nil {
-			return nil, err
-		}
-
-		car := new(Car)
-		_ = json.Unmarshal(queryResponse.Value, car)
-
-		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
-		results = append(results, queryResult)
+	if Bvalbytes == nil {
+		return fmt.Errorf("Entity not found")
 	}
+	Bval, _ = strconv.Atoi(string(Bvalbytes))
 
-	return results, nil
-}
+	// Perform the execution
+	Aval = Aval - X
+	Bval = Bval + X
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
 
-// ChangeCarOwner updates the owner field of car with given id in world state
-func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
-	car, err := s.QueryCar(ctx, carNumber)
-
+	// Write the state back to the ledger
+	err = ctx.GetStub().PutState(A, []byte(strconv.Itoa(Aval)))
 	if err != nil {
 		return err
 	}
 
-	car.Owner = newOwner
+	err = ctx.GetStub().PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return err
+	}
 
-	carAsBytes, _ := json.Marshal(car)
+	return nil
+}
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+// Delete  an entity from state
+func (t *ABstore) Delete(ctx contractapi.TransactionContextInterface, A string) error {
+
+	// Delete the key from the state in ledger
+	err := ctx.GetStub().DelState(A)
+	if err != nil {
+		return fmt.Errorf("Failed to delete state")
+	}
+
+	return nil
+}
+
+// Query callback representing the query of a chaincode
+func (t *ABstore) Query(ctx contractapi.TransactionContextInterface, A string) (string, error) {
+	var err error
+	// Get the state from the ledger
+	Avalbytes, err := ctx.GetStub().GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		return "", errors.New(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+		return "", errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return string(Avalbytes), nil
 }
 
 func main() {
-
-	chaincode, err := contractapi.NewChaincode(new(SmartContract))
-
+	cc, err := contractapi.NewChaincode(new(ABstore))
 	if err != nil {
-		fmt.Printf("Error create fabcar chaincode: %s", err.Error())
-		return
+		panic(err.Error())
 	}
-
-	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
+	if err := cc.Start(); err != nil {
+		fmt.Printf("Error starting ABstore chaincode: %s", err)
 	}
 }
